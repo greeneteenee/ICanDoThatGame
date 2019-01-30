52018ffcf410b7d3f4574fa8232dd8dd520b93ee
Tina Greene <baby_phat03@msn.com>
2019-01-30 14:37:13 -0800

    Added ICanDoThatDB database script to project

---
+++ ICanDoThatDB/ICanDoThatScript.sql
@@ -0,0 +1,33 @@
+﻿use master
+GO
+
+IF DB_ID('ICanDoThatDB') IS NOT NULL
+	DROP Database ICanDoThatDB
+GO
+
+CREATE Database ICanDoThatDB
+GO
+
+Use ICanDoThatDB
+GO
+
+CREATE TABLE ActionItems
+(
+	ActionID int PRIMARY KEY IDENTITY
+	,ActionDescription varchar(50) NOT NULL
+)
+GO
+
+CREATE TABLE WhereItems
+(
+	WhereID int PRIMARY KEY IDENTITY
+	,WhereDescription varchar(50) NOT NULL
+)
+GO
+
+CREATE TABLE WithItems
+(
+	WithID int PRIMARY KEY IDENTITY
+	,WithDescription varchar(50) NOT NULL
+)
+GO
