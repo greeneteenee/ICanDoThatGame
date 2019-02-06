39fc7d7b7ab5a7f8cf2c691ee4cd50af57164d10
Tina Greene <46331543+greeneteenee@users.noreply.github.com>
2019-02-06 11:01:55 -0800

    Created ActionItemsDB class and added INSERT functionality

---
+++ ICanDoThatGame/ActionItemDB.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICanDoThatGame
+{
+    static class ActionItemDB
+    {
+        /// <summary>
+        /// This method adds an action item to the Actionitems table (INSERT functionality)
+        /// </summary>
+        /// <param name="item"></param>
+        public static void AddActionItem(ActionItem item)
+        {
+            //database connection
+            GameDB db = new GameDB();
+
+            //
+            db.ActionItems.Add(item);
+            db.SaveChanges();   
+
+        }
+    }
+}
