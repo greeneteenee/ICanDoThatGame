870a0e435efbfec7516ae851b3933cd54dc82ee7
Tina Greene <46331543+greeneteenee@users.noreply.github.com>
2019-03-05 22:24:12 -0800

    Started unit tests

---
+++ ICanDoThatGameTests/GameTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ICanDoThatGame;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICanDoThatGame.Tests
+{
+    [TestClass()]
+    public class GameTests
+    {
+        [TestMethod()]
+        public void OutOfTurnsTestMethod_NumTurnsLessThanMax_ReturnFalse()
+        {
+            //Arrange
+            int maxTurns = 10;
+            int numTurns = 5;
+
+            //Act
+            if(numTurns < maxTurns)
+            {
+                bool result = true;
+            }
+
+        }
+    }
+}
